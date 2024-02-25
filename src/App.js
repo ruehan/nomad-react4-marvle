805c5b2fc8c6733e0dfc16c52faa492e2ec16ade
vendredl <node.js_@kakao.com>
2024-02-25 14:44:04 +0900

    :speech_balloon: Darkmode localstorage 저장

--- src/App.js
+++ src/App.js
@@ -3,14 +3,26 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 import { darkTheme, lightTheme } from '../src/utils/theme';
 import { darkMode } from '../src/utils/atom';
-import { useRecoilValue } from 'recoil';
 import Home from './pages/Home';
 import CharacterInfoPage from './pages/CharacterInfoPage';
 import Header from './components/Header';
 import AboutPage from './pages/AboutPage';
+import { useEffect } from 'react';
+import { useRecoilState } from 'recoil';
+
+
 
 function App() {
-  const darkmode = useRecoilValue(darkMode);
+  const [darkmode, setDarkmode] = useRecoilState(darkMode);
+
+  useEffect(() => {
+    const isDarkMode = localStorage.getItem('darkMode') === 'true';
+    setDarkmode(isDarkMode);
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('darkMode', darkmode);
+  }, [darkmode]);
 
   return (
     <ThemeProvider theme={darkmode ? darkTheme : lightTheme}>
